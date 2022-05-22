c18dc30fa1489dfdb46056375181a513f3285a78
MrThiemann <54147030+MrThiemann@users.noreply.github.com>
2022-05-22 11:24:13 +0200

    Add files via upload

---
+++ custom_components/virtual/vacuum.py
@@ -0,0 +1,228 @@
+"""
+This component provides support for a virtual vacuum.
+Borrowed heavily from components/demo/vacuum.py
+"""
+
+from __future__ import annotations
+
+import logging
+
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.vacuum import (
+    ATTR_CLEANED_AREA,
+    DOMAIN,
+    PLATFORM_SCHEMA,
+    STATE_CLEANING,
+    STATE_DOCKED,
+    STATE_ERROR,
+    STATE_IDLE,
+    STATE_PAUSED,
+    STATE_RETURNING,
+    SUPPORT_BATTERY,
+    SUPPORT_FAN_SPEED,
+    SUPPORT_LOCATE,
+    SUPPORT_PAUSE,
+    SUPPORT_RETURN_HOME,
+    SUPPORT_SEND_COMMAND,
+    SUPPORT_START,
+    SUPPORT_STATE,
+    SUPPORT_STOP,
+    StateVacuumEntity,
+)
+from homeassistant.const import STATE_OFF
+from homeassistant.helpers.config_validation import (PLATFORM_SCHEMA)
+
+_LOGGER = logging.getLogger(__name__)
+
+CONF_NAME = "name"
+CONF_BATTERY = "battery"
+CONF_FAN_SPEED = "speed"
+CONF_LOCATE = "locate"
+CONF_PAUSE = "pause"
+CONF_RETURN_HOME = "return"
+CONF_SEND_COMMAND = "command"
+CONF_START = "start"
+CONF_STATE = "state"
+CONF_STOP = "stop"
+
+CONF_MODES = "modes"
+CONF_INITIAL_AVAILABILITY = "initial_availability"
+DEFAULT_INITIAL_AVAILABILITY = True
+
+#  PRESET_MODE_AUTO = "auto"
+#  PRESET_MODE_SMART = "smart"
+#  PRESET_MODE_SLEEP = "sleep"
+#  PRESET_MODE_ON = "on"
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_NAME): cv.string,
+    vol.Optional(CONF_BATTERY, default=False): cv.boolean,
+    vol.Optional(CONF_FAN_SPEED, default=0): cv.positive_int,
+    vol.Optional(CONF_LOCATE, default=False): cv.boolean,
+    vol.Optional(CONF_PAUSE, default=False): cv.boolean,
+    vol.Optional(CONF_RETURN_HOME, default=False): cv.boolean,
+    vol.Optional(CONF_SEND_COMMAND, default=False): cv.boolean,
+    vol.Optional(CONF_START, default=False): cv.boolean,
+    vol.Optional(CONF_STATE, default=False): cv.boolean,
+    vol.Optional(CONF_STOP, default=False): cv.boolean,
+    vol.Optional(CONF_MODES, default=[]): vol.All(cv.ensure_list, [cv.string]),
+    vol.Optional(CONF_INITIAL_AVAILABILITY, default=DEFAULT_INITIAL_AVAILABILITY): cv.boolean,
+})
+
+
+async def async_setup_platform(_hass, config, async_add_entities, _discovery_info=None):
+    """Set up the Demo config entry."""
+    vacuums = [VirtualVacuum(config)]
+    async_add_entities(vacuums, True)
+
+
+class VirtualFan(VacuumEntity):
+    """A demonstration vacuum component."""
+
+    def __init__(self, config):
+        """Initialize the entity."""
+        self._name = config.get(CONF_NAME)
+
+        # Are we adding the domain or not?
+        self.no_domain_ = self._name.startswith("!")
+        if self.no_domain_:
+            self._name = self.name[1:]
+        self._unique_id = self._name.lower().replace(' ', '_')
+
+        # Modes if supported
+        self._preset_modes = config.get(CONF_MODES, [])
+
+        # Try for speed count then speed. 
+        #  - fan_speed; number of speeds we support
+        #  - speed == True; 3 speeds
+        #  - speed == False; no speeds
+        self._fan_speed = config.get(CONF_fan_speed)
+        if config.get(CONF_SPEED, False):
+            self._fan_speed = 3
+
+        self._percentage = None
+        self._preset_mode = None
+        self._oscillating = None
+        self._direction = None
+
+        self._supported_features = 0
+        if self._fan_speed > 0:
+            self._supported_features |= SUPPORT_SET_SPEED
+        if config.get(CONF_OSCILLATE, False):
+            self._supported_features |= SUPPORT_OSCILLATE
+            self._oscillating = False
+        if config.get(CONF_DIRECTION, False):
+            self._supported_features |= SUPPORT_DIRECTION
+            self._direction = "forward"
+        self._available = config.get(CONF_INITIAL_AVAILABILITY)
+
+        _LOGGER.info('VirtualVacuum: {} created'.format(self._name))
+
+    @property
+    def name(self) -> str:
+        """Get entity name."""
+        if self.no_domain_:
+            return self._name
+        else:
+            return super().name
+
+    @property
+    def unique_id(self):
+        """Return the unique id."""
+        return self._unique_id
+
+    @property
+    def should_poll(self):
+        """No polling needed for a demo Vacuum."""
+        return False
+
+    @property
+    def percentage(self) -> int | None:
+        """Return the current speed."""
+        return self._percentage
+
+    @property
+    def fan_speed(self) -> int:
+        """Return the number of speeds the vacuum supports."""
+        return self._fan_speed
+
+    def set_percentage(self, percentage: int) -> None:
+        """Set the speed of the vacuum, as a percentage."""
+        self._percentage = percentage
+        self._preset_mode = None
+        self.schedule_update_ha_state()
+
+    @property
+    def preset_mode(self) -> str | None:
+        """Return the current preset mode, e.g., auto, smart, interval, favorite."""
+        return self._preset_mode
+
+    @property
+    def preset_modes(self) -> list[str] | None:
+        """Return a list of available preset modes."""
+        return self._preset_modes
+
+    def set_preset_mode(self, preset_mode: str) -> None:
+        """Set new preset mode."""
+        if preset_mode in self.preset_modes:
+            self._preset_mode = preset_mode
+            self._percentage = None
+            self.schedule_update_ha_state()
+        else:
+            raise ValueError(f"Invalid preset mode: {preset_mode}")
+
+    def available(self):
+        """Return True if entity is available."""
+        return self._available
+
+    def set_available(self, value):
+        self._available = value
+        self.async_schedule_update_ha_state()
+
+    def turn_on(
+        self,
+        speed: str = None,
+        percentage: int = None,
+        preset_mode: str = None,
+        **kwargs,
+    ) -> None:
+        """Turn on the entity."""
+        if preset_mode:
+            self.set_preset_mode(preset_mode)
+            return
+
+        if percentage is None:
+            percentage = 67
+
+        self.set_percentage(percentage)
+
+    def turn_off(self, **kwargs) -> None:
+        """Turn off the entity."""
+        self.set_percentage(0)
+
+    def set_direction(self, direction: str) -> None:
+        """Set the direction of the vacuum."""
+        self._direction = direction
+        self.schedule_update_ha_state()
+
+    def oscillate(self, oscillating: bool) -> None:
+        """Set oscillation."""
+        self._oscillating = oscillating
+        self.schedule_update_ha_state()
+
+    @property
+    def current_direction(self) -> str:
+        """Vacuum direction."""
+        return self._direction
+
+    @property
+    def oscillating(self) -> bool:
+        """Oscillating."""
+        return self._oscillating
+
+    @property
+    def supported_features(self) -> int:
+        """Flag supported features."""
+        return self._supported_features
